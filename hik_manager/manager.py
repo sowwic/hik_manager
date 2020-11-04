78c3ff4851b6f4a3e58bafb5777fce8d9635f73f
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2020-11-04 22:40:06 +0000

    Add definition lock method

--- hik_manager/manager.py
+++ hik_manager/manager.py
@@ -10,17 +10,18 @@ class HIKManager:
         if character_node:
             self.character_node = pm.PyNode(character_node)
 
+    @staticmethod
+    def toggle_lock_definition():
+        pm.mel.eval("evalDeferred hikToggleLockDefinition;")
+
     def export_definition(self, path):
         # Serialize skeleton definition
         self.properties_node = self.character_node.propertyState.listConnections(d=1)[0]  # type: pm.PyNode
         export_data = {}
         definition_data = {}
         properties_data = {}
-        for connection in self.character_node.listConnections(c=1, d=1):
-            if connection[0].attrName() == "propertyState":
-                continue
+        for connection in self.character_node.listConnections(c=1, t="joint"):
             definition_data[connection[0].attrName()] = connection[1].name()
-
         # Serialize HIK properties
         for attr in pm.listAttr(self.properties_node, se=1, o=1):
             properties_data[attr] = pm.getAttr("{0}.{1}".format(self.properties_node, attr))
@@ -33,7 +34,7 @@ class HIKManager:
 
         Logger.info("Exported {0} definition to: {1}".format(str(self.character_node), path))
 
-    def import_definition(self, path):
+    def import_definition(self, path, lock=True):
         # Create new HIK node if one is not setup
         if not self.character_node:
             self.character_node = pm.createNode("HIKCharacterNode")  # type: pm.PyNode
@@ -60,10 +61,22 @@ class HIKManager:
 
         # Apply properties data
         for attr, value in properties.items():
-            pm.setAttr("{0}.{1}".format(self.properties_node, attr), value)
+            try:
+                pm.setAttr("{0}.{1}".format(self.properties_node, attr), value)
+            except Exception:
+                Logger.exception("Failed to setAttr: {0}, {1}".format(attr, value))
 
+        # Update UI and lock
         pm.mel.eval('hikUpdateContextualUI()')
+        if lock:
+            HIKManager.toggle_lock_definition()
         Logger.info("Imported HIK definition: {0}".format(path))
+
+    def export_custom_rig(self, path):
+        pass
+
+    def import_custom_rig(self, path):
+        pass
 
 
 if __name__ == "__main__":
